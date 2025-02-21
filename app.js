855c72564c8ec82dab24383e13048cec23c8a41c
codmos <codecosmos0@gmail.com>
2025-02-21 13:26:11 +0500

    Added dotenv and PORT config

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 const app = express()
 app.use(express.json())
